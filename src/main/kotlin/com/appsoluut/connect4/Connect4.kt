813e2537426ce7e6bcae6376253de5570288a367
Paul Hameteman <paul.hameteman@ns.nl>
2026-01-21 05:19:13 +0100

    R

--- src/main/kotlin/com/appsoluut/connect4/Connect4.kt
+++ src/main/kotlin/com/appsoluut/connect4/Connect4.kt
@@ -3,7 +3,5 @@ package com.appsoluut.connect4
 class Connect4 {
     private val board = Board()
 
-    fun getBoard(): Board = board
-
     fun isEmptyBoard() = board.isEmpty()
 }
--- src/test/kotlin/com/appsoluut/connect4/Connect4Test.kt
+++ src/test/kotlin/com/appsoluut/connect4/Connect4Test.kt
@@ -5,17 +5,17 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-@DisplayName("Connect4")
+@DisplayName("Connect4 should")
 class Connect4Test {
     @Test
-    @DisplayName("should create a empty board with 6 rows and 7 columns")
+    @DisplayName("create a empty board with 6 rows and 7 columns")
     fun emptyBoardWith6RowsAnd7Columns() {
         assertEquals(6, Board.BOARD_ROWS, "Board should have 6 rows")
         assertEquals(7, Board.BOARD_COLUMNS, "Board should have 7 columns")
     }
 
     @Test
-    @DisplayName("should create a empty board and all fields are empty")
+    @DisplayName("create a empty board and all fields are empty")
     fun emptyBoard() {
         val game = Connect4()
         assertTrue(game.isEmptyBoard(), "All board fields should be empty")
