8288c2b455825d6918c5a48599df6bb642ebeec1
Paul Hameteman <paul.hameteman@ns.nl>
2026-01-21 05:39:23 +0100

    R

--- src/main/kotlin/com/appsoluut/connect4/Board.kt
+++ src/main/kotlin/com/appsoluut/connect4/Board.kt
@@ -2,4 +2,5 @@ package com.appsoluut.connect4
 
 class Board {
     val rows = 6
+    val columns = 7
 }
--- src/test/kotlin/com/appsoluut/connect4/BoardTest.kt
+++ src/test/kotlin/com/appsoluut/connect4/BoardTest.kt
@@ -8,8 +8,15 @@ import kotlin.test.assertEquals
 class BoardTest {
     @Test
     @DisplayName("have 6 rows")
-    fun boardHas6Rows() {
+    fun has6Rows() {
         val board = Board()
         assertEquals(6, board.rows)
+    }
+
+    @Test
+    @DisplayName("have 7 columns")
+    fun has7Columns() {
+        val board = Board()
+        assertEquals(7, board.columns)
     }
 }
