61dc035e74a2b00bd6c912b30ae7d0d753bd4af9
Paul Hameteman <paul.hameteman@ns.nl>
2026-01-21 07:31:27 +0100

    R

--- src/main/kotlin/com/appsoluut/connect4/Board.kt
+++ src/main/kotlin/com/appsoluut/connect4/Board.kt
@@ -2,17 +2,11 @@ package com.appsoluut.connect4
 
 class Board {
     val rows = 6
-
     val columns = 7
     val positions = List(rows * columns) { Position(column = it % columns, row = it / columns) }
 
     fun positionAt(
         row: Int,
         column: Int,
-    ): Position {
-        return Position(
-            row = row,
-            column = column,
-        )
-    }
+    ): Position = positions[row * columns + column]
 }
