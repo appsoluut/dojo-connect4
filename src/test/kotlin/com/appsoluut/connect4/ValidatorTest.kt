38987d5814ba7c5f316d46052781544b1b0b285a
Paul Hameteman <paul.hameteman@ns.nl>
2026-03-09 17:05:02 +0100

    T: Player 2 coin out of valid range

--- src/test/kotlin/com/appsoluut/connect4/ValidatorTest.kt
+++ src/test/kotlin/com/appsoluut/connect4/ValidatorTest.kt
@@ -1,5 +1,6 @@
 package com.appsoluut.connect4
 
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import kotlin.test.assertTrue
@@ -14,5 +15,15 @@ class ValidatorTest {
         val input = 4
 
         assertTrue(validator.isColumnValid(input))
+    }
+
+    @Test
+    @DisplayName("check if column number is out of valid range")
+    fun checkColumnOutOfRange() {
+        val validator = Validator(Board.empty())
+
+        val input = 9
+
+        assertFalse(validator.isColumnValid(input))
     }
 }
