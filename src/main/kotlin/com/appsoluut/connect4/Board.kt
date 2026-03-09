69c798d211ff9e98e77da57c9c31d367cdde5e9d
Paul Hameteman <paul.hameteman@ns.nl>
2026-03-09 12:12:20 +0100

    R: back to 7 columns

--- src/main/kotlin/com/appsoluut/connect4/Board.kt
+++ src/main/kotlin/com/appsoluut/connect4/Board.kt
@@ -28,7 +28,7 @@ class Board private constructor(
 
     companion object {
         const val ROWS = 6
-        const val COLUMNS = 8
+        const val COLUMNS = 7
 
         fun empty(
             rows: Int = ROWS,
