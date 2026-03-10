5598cd4f7514b93a6c05196ee72445743e93da56
Paul Hameteman <paul.hameteman@ns.nl>
2026-03-10 21:36:25 +0100

    T: Vertical win

--- src/main/kotlin/com/appsoluut/connect4/WinCondition.kt
+++ src/main/kotlin/com/appsoluut/connect4/WinCondition.kt
@@ -10,7 +10,7 @@ class WinCondition {
             val coin = board.positionAt(row = position.row, column = column).coin
             if (coin == position.coin) {
                 consecutive++
-                if (consecutive == 4) {
+                if (consecutive == CONSECUTIVE_COINS) {
                     return true
                 }
             } else {
@@ -18,5 +18,28 @@ class WinCondition {
             }
         }
         return false
+    }
+
+    fun checkVerticalWin(
+        board: Board,
+        position: Position,
+    ): Boolean {
+        var consecutive = 0
+        for (row in 1..board.rows) {
+            val coin = board.positionAt(row = row, column = position.column).coin
+            if (coin == position.coin) {
+                consecutive++
+                if (consecutive == CONSECUTIVE_COINS) {
+                    return true
+                }
+            } else {
+                consecutive = 0
+            }
+        }
+        return false
+    }
+
+    companion object {
+        const val CONSECUTIVE_COINS = 4
     }
 }
--- src/test/kotlin/com/appsoluut/connect4/WinConditionTest.kt
+++ src/test/kotlin/com/appsoluut/connect4/WinConditionTest.kt
@@ -46,7 +46,51 @@ class WinConditionTest {
         board = board.placeCoinAt(Position(row = 2, column = startColumn + 3, coin = Coin.Yellow))
         board = board.placeCoinAt(Position(row = 2, column = startColumn + 4, coin = Coin.Yellow))
         val lastPosition = Position(row = 2, column = startColumn + 5, coin = Coin.Yellow)
+        board = board.placeCoinAt(lastPosition)
 
         assertFalse(winCondition.checkHorizontalWin(board, lastPosition))
+    }
+
+    @Test
+    @DisplayName("check if 4 of the same coins are in a vertical position")
+    fun verticalWin() {
+        val winCondition = WinCondition()
+        val coin = Coin.Red
+        val column = 3
+
+        var board = Board.empty()
+        var position =
+            Position(
+                row = 1,
+                column = column,
+                coin = coin,
+            )
+        for (row in 1..4) {
+            position =
+                Position(
+                    row = row,
+                    column = column,
+                    coin = coin,
+                )
+            board = board.placeCoinAt(position)
+        }
+
+        assertTrue(winCondition.checkVerticalWin(board, position))
+    }
+
+    @Test
+    @DisplayName("check if vertical win condition is not met when there are not 4 of the same coins in the same column")
+    fun verticalWinNotMet() {
+        val winCondition = WinCondition()
+
+        var board = Board.empty()
+        board = board.placeCoinAt(Position(row = 1, column = 2, coin = Coin.Yellow))
+        board = board.placeCoinAt(Position(row = 2, column = 2, coin = Coin.Red))
+        board = board.placeCoinAt(Position(row = 3, column = 2, coin = Coin.Yellow))
+        board = board.placeCoinAt(Position(row = 4, column = 2, coin = Coin.Yellow))
+        val lastPosition = Position(row = 5, column = 2, coin = Coin.Yellow)
+        board = board.placeCoinAt(lastPosition)
+
+        assertFalse(winCondition.checkVerticalWin(board, lastPosition))
     }
 }
